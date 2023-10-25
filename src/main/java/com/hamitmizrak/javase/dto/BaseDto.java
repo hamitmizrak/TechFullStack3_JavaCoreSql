9fe72d5e52f31ffb63dd28400ce1d9cb7ca2ce63
HamitMizrak <hamitmizrak@gmail.com>
2023-10-25 12:49:30 +0300

    DTO

---
+++ src/main/java/com/hamitmizrak/javase/dto/BaseDto.java
@@ -0,0 +1,12 @@
+package com.hamitmizrak.javase.dto;
+
+import java.util.Date;
+
+public class BaseDto {
+
+    // Variable
+    private Long id;
+    private Date systemCreatedDate;
+
+    // Getter And Setter
+}
