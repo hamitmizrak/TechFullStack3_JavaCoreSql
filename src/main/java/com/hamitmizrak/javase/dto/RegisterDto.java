e41a4faefb3065856ff9eb92d8c02d4e040fc314
HamitMizrak <hamitmizrak@gmail.com>
2023-10-27 14:33:54 +0300

    RegisterDto

--- src/main/java/com/hamitmizrak/javase/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/javase/dto/RegisterDto.java
@@ -1,10 +1,111 @@
 package com.hamitmizrak.javase.dto;
 
+import java.io.Serializable;
+
 // Register
-public class RegisterDto extends BaseDto {
+public class RegisterDto extends BaseDto implements Serializable {
+
+    // Serileştirme
+    public static final Long serialVersionUID=1L;
+
+    //Variable
+    private String uNickname;
+    private String uEmailAddress;
+    private String uPassword;
+    private int remainingNumber; // Kullanıcı kalan hak
+    private Boolean isPassive;
+    private String rolles;
+    private int counter;
+
+    // constructor(parametresiz)
+    public RegisterDto() {
+    }
+
+    // constructor(parametreli)
+    public RegisterDto(String uNickname, String uEmailAddress, String uPassword, int remainingNumber, Boolean isPassive, String rolles) {
+        this.uNickname = uNickname;
+        this.uEmailAddress = uEmailAddress;
+        this.uPassword = uPassword;
+        this.remainingNumber = remainingNumber;
+        this.isPassive = isPassive;
+        this.rolles = rolles;
+    }
+
+    //toString
+
+
+    @Override
+    public String toString() {
+        return "RegisterDto{" +
+                "uNickname='" + uNickname + '\'' +
+                ", uEmailAddress='" + uEmailAddress + '\'' +
+                ", uPassword='" + uPassword + '\'' +
+                ", remainingNumber=" + remainingNumber +
+                ", isPassive=" + isPassive +
+                ", rolles='" + rolles + '\'' +
+                ", counter=" + counter +
+                "} " + super.toString();
+    }
 
     @Override
     public String nowDateAbstract() {
         return null;
+    }
+
+    //GETTER AND SETTER
+    public String getuNickname() {
+        return uNickname;
+    }
+
+    public void setuNickname(String uNickname) {
+        this.uNickname = uNickname;
+    }
+
+    public String getuEmailAddress() {
+        return uEmailAddress;
+    }
+
+    public void setuEmailAddress(String uEmailAddress) {
+        this.uEmailAddress = uEmailAddress;
+    }
+
+    public String getuPassword() {
+        return uPassword;
+    }
+
+    public void setuPassword(String uPassword) {
+        this.uPassword = uPassword;
+    }
+
+    public int getRemainingNumber() {
+        return remainingNumber;
+    }
+
+    public void setRemainingNumber(int remainingNumber) {
+        this.remainingNumber = remainingNumber;
+    }
+
+    public Boolean getPassive() {
+        return isPassive;
+    }
+
+    public void setPassive(Boolean passive) {
+        isPassive = passive;
+    }
+
+    public String getRolles() {
+        return rolles;
+    }
+
+    public void setRolles(String rolles) {
+        this.rolles = rolles;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
